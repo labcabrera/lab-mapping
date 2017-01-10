1943443e4b0ef8644ed695c65e18c320671fad01
lab <lab@192.168.200.194>
2017-01-10 17:23:41 +0100

    Spring API converter performance test

--- src/test/java/org/lab/commons/training/spring/config/SpringTrainingConfig.java
+++ src/test/java/org/lab/commons/training/spring/config/SpringTrainingConfig.java
@@ -3,6 +3,7 @@ package org.lab.commons.training.spring.config;
 import org.lab.commons.mapper.EnableSpringConversionService;
 import org.lab.commons.training.spring.mapping.CustomerToCustomerDtoConverter;
 import org.lab.commons.training.spring.mapping.CustomerToPersonConverter;
+import org.lab.commons.training.spring.mapping.HugeBeanConverter;
 import org.lab.commons.training.spring.mapping.PersonToCustomerConverter;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableSpringConversionService(autoScan = false, //
 		converters = { PersonToCustomerConverter.class, //
 				CustomerToCustomerDtoConverter.class, //
-				CustomerToPersonConverter.class })
+				CustomerToPersonConverter.class, //
+				HugeBeanConverter.class })
 public class SpringTrainingConfig {
 
 }
---
+++ src/test/java/org/lab/commons/training/spring/mapping/HugeBeanConverter.java
@@ -0,0 +1,24 @@
+package org.lab.commons.training.spring.mapping;
+
+import org.lab.commons.training.common.domain.HugeBean;
+import org.lab.commons.training.common.domain.HugeBeanCopy;
+import org.springframework.core.convert.converter.Converter;
+
+public class HugeBeanConverter implements Converter<HugeBean, HugeBeanCopy> {
+
+	@Override
+	public HugeBeanCopy convert(HugeBean source) {
+		HugeBeanCopy result = new HugeBeanCopy();
+		result.setStr01(source.getStr01());
+		result.setStr02(source.getStr02());
+		result.setStr03(source.getStr03());
+		result.setStr04(source.getStr04());
+		result.setStr05(source.getStr05());
+		result.setStr06(source.getStr06());
+		result.setStr07(source.getStr07());
+		result.setStr08(source.getStr08());
+		result.setStr09(source.getStr09());
+		result.setStr10(source.getStr10());
+		return result;
+	}
+}
