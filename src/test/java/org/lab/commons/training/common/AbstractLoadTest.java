72d2f29b05d45897b976bfe7bdefdb33d9992e45
lab <lab@192.168.200.194>
2017-01-10 12:42:27 +0100

    Initial load test

--- src/test/java/org/lab/commons/training/common/AbstractLoadTest.java
+++ src/test/java/org/lab/commons/training/common/AbstractLoadTest.java
@@ -2,4 +2,7 @@ package org.lab.commons.training.common;
 
 public class AbstractLoadTest {
 
+	public static void main(String[] args) {
+
+	}
 }
---
+++ src/test/java/org/lab/commons/training/common/domain/HorribleBean.java
@@ -0,0 +1,54 @@
+package org.lab.commons.training.common.domain;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class HorribleBean {
+
+	private String str01;
+	private String str02;
+	private String str03;
+	private String str04;
+	private String str05;
+	private String str06;
+	private String str07;
+	private String str08;
+	private String str09;
+	private String str10;
+
+	private Integer int01;
+	private Integer int02;
+	private Integer int03;
+	private Integer int04;
+	private Integer int05;
+	private Integer int06;
+	private Integer int07;
+	private Integer int08;
+	private Integer int09;
+	private Integer int10;
+	
+	private BigDecimal bigDecimal01;
+	private BigDecimal bigDecimal02;
+	private BigDecimal bigDecimal03;
+	private BigDecimal bigDecimal04;
+	private BigDecimal bigDecimal05;
+	private BigDecimal bigDecimal06;
+	private BigDecimal bigDecimal07;
+	private BigDecimal bigDecimal08;
+	private BigDecimal bigDecimal09;
+	private BigDecimal bigDecimal10;
+
+	private Address address01;
+	private Address address02;
+	private Address address03;
+	private Address address04;
+	private Address address05;
+	private Address address06;
+	private Address address07;
+	private Address address08;
+	private Address address09;
+	private Address address10;
+
+}
